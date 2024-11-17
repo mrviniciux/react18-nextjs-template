6b8d479a8e0abfad85f046cace5df7df3a52740e
mrviniciux <httpmarks@gmail.com>
2024-11-17 14:39:09 -0300

    feat(routing): force source / to /pt-BR

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,15 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  redirects: async () => {
+    return [
+      {
+        source: '/',
+        destination: '/pt-BR/',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default withNextIntl(nextConfig);
